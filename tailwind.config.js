334cdf6038eb5089de7a7a16b95b885efc5ae28f
MauricioH <mauricioh12345678912@gmail.com>
2025-01-18 11:49:26 -0500

    instalacion de Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [    
+    "./views/**/*.html", // Escanea archivos HTML dentro de la carpeta views
+    "./js/**/*.js", ],
+    // Escanea archivos JS dentro de la carpeta js],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
