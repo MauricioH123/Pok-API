ca3bf85fd2d7a9ca8ab064924939d3e263f2d7f6
MauricioH <mauricioh12345678912@gmail.com>
2025-01-21 18:44:05 -0500

    detalles del pokemon v1

--- js/scriptPaginaPokemon.js
+++ js/scriptPaginaPokemon.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const currentUrl = window.location.href;
     console.log(currentUrl); // Muestra la URL completa en la consola
+    const urlP = new URL(currentUrl);
+    const searchPokemon = urlP.search;
+    const idPokemon = searchPokemon[4];
 
-    
+    async function obtenerPokemon(id){
+        let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        let resultado = await pokemon.json();
+        console.log(resultado.forms[0].name)
+    }
+
+    obtenerPokemon(idPokemon);
 })
