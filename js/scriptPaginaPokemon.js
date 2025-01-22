75f53cd08fe39a2751ca9eb7797fff5cbc380d63
MauricioH <mauricioh12345678912@gmail.com>
2025-01-21 20:22:09 -0500

    detalles de pokemon v3

--- js/scriptPaginaPokemon.js
+++ js/scriptPaginaPokemon.js
@@ -10,12 +10,122 @@ document.addEventListener("DOMContentLoaded", function () {
         try {
             let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
             let resultado = await pokemon.json();
-            console.log(resultado.forms[0].name);
+            // console.log(resultado.forms[0].name);
             return resultado;
         } catch (error) {
             console.log("Error en la obtencion del pokemon", error);
 
         }
+
+    }
+
+    async function obtenerAtributos(id){
+        const jsonResultados = await obtenerPokemon(id);
+        const estadisticas = jsonResultados.stats;
+        const hp = estadisticas[0].base_stat;
+        const attack = estadisticas[1].base_stat;
+        const defense = estadisticas[2].base_stat;
+        const special_attack = estadisticas[3].base_stat;
+        const special_defense = estadisticas[4].base_stat;
+        const speed = estadisticas[5].base_stat;
+
+
+        const options = {
+            colors: ["#1A56DB", "#FDBA8C"],
+            series: [
+              {
+                name: "Organic",
+                color: "#1A56DB",
+                data: [
+                  { x: "HP", y: hp },
+                  { x: "Ataque", y: attack },
+                  { x: "Defensa", y: defense },
+                  { x: "Ataque especial", y: special_attack },
+                  { x: "Defensa especial", y: special_defense },
+                  { x: "Velocidad", y: speed },
+                ],
+              }
+            ],
+            chart: {
+              type: "bar",
+              height: "320px",
+              fontFamily: "Inter, sans-serif",
+              toolbar: {
+                show: false,
+              },
+            },
+            plotOptions: {
+              bar: {
+                horizontal: false,
+                columnWidth: "70%",
+                borderRadiusApplication: "end",
+                borderRadius: 8,
+              },
+            },
+            tooltip: {
+              shared: true,
+              intersect: false,
+              style: {
+                fontFamily: "Inter, sans-serif",
+              },
+            },
+            states: {
+              hover: {
+                filter: {
+                  type: "darken",
+                  value: 1,
+                },
+              },
+            },
+            stroke: {
+              show: true,
+              width: 0,
+              colors: ["transparent"],
+            },
+            grid: {
+              show: false,
+              strokeDashArray: 4,
+              padding: {
+                left: 2,
+                right: 2,
+                top: -14
+              },
+            },
+            dataLabels: {
+              enabled: false,
+            },
+            legend: {
+              show: false,
+            },
+            xaxis: {
+              floating: false,
+              labels: {
+                show: true,
+                style: {
+                  fontFamily: "Inter, sans-serif",
+                  cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+                }
+              },
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+            },
+            yaxis: {
+              show: false,
+            },
+            fill: {
+              opacity: 1,
+            },
+          }
+          
+          if(document.getElementById("column-chart") && typeof ApexCharts !== 'undefined') {
+            const chart = new ApexCharts(document.getElementById("column-chart"), options);
+            chart.render();
+          }
+          
 
     }
 
@@ -30,5 +140,8 @@ document.addEventListener("DOMContentLoaded", function () {
         imagenPokemon.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
     }
 
+    obtenerAtributos(idPokemon);
     crearDetalles(idPokemon);
+
+    
 })
