01f0e17ff088975b2a7e495e273ee5a448895e90
MauricioH <mauricioh12345678912@gmail.com>
2025-01-21 19:08:01 -0500

    detalles de pokemones v2

--- js/scriptPaginaPokemon.js
+++ js/scriptPaginaPokemon.js
@@ -6,11 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const searchPokemon = urlP.search;
     const idPokemon = searchPokemon[4];
 
-    async function obtenerPokemon(id){
-        let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        let resultado = await pokemon.json();
-        console.log(resultado.forms[0].name)
+    async function obtenerPokemon(id) {
+        try {
+            let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+            let resultado = await pokemon.json();
+            // console.log(resultado.forms[0].name);
+            return resultado;
+        } catch (error) {
+            console.log("Error en la obtencion del pokemon", error);
+
+        }
+
     }
 
-    obtenerPokemon(idPokemon);
+    async function crearDetalles(id) {
+        let jsonDetalles = await obtenerPokemon(id);
+        const nombrePokemon = document.querySelector("#nombrePokemon");
+        const parrafoPokemon = document.querySelector("#parrafoPokemon");
+        const imagenPokemon = document.querySelector("#imagenPokemon");
+        
+        nombrePokemon.textContent = `${jsonDetalles.forms[0].name}`;
+        parrafoPokemon.textContent = `detalles del pokemon`;
+        imagenPokemon.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    }
+
+    crearDetalles(idPokemon);
 })
